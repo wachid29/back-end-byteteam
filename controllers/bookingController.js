fefb7fbc07fd5f04d87b531413434febfde979e4
wachid <wachidnurafif29@gmail.com>
2022-08-18 19:14:12 +0700

    [edit] controller booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -162,6 +162,11 @@ const deletebyid = async (req, res) => {
 
 const statuspaymentV2 = async (req, res) => {
   try {
+    const user_role = req.tokenUserRole;
+    if (user_role != "admin") {
+      return res.status(400).send(`Can't access, your id role is 'customer'`);
+    }
+
     const { id_booking, status_payment } = req.body;
     // const id_user = req.tokenUserId;
 
