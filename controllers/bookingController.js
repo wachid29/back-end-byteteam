3b93d70e1d5d5cec137c2d54741231a12c00e803
pitlimitCode <prabow14@gmail.com>
2022-08-19 13:33:58 +0700

    [edit] post & patch booking: validate, auth by role

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -36,67 +36,36 @@ const getbyiduser = async (req, res) => {
   }
 };
 
-// POST DATA TO BOOKINGS TABLE BY ID_BOOKING
+// POST DATA TO BOOKINGS TABLE BY ID_BOOKING, just role "customer" can Access.
 const post = async (req, res) => {
   try {
-    // Validation, just role 'customer' can Access.
-    const user_role = req.tokenUserRole;
-    if (user_role != "customer") {
-      return res.status(400).send(`Can't access, your id role is 'admin'`);
-    }
+    const { id_user, user_role, id_ticket, total_passenger, total_payment } = req.body;
 
-    const id_user = req.tokenUserId;
-    if (id_user == "") {
-      return res.status(400).send(`Please input id_user`);
-    }
+    if (user_role != "customer") { return res.status(400).send(`Can't access, your id role must be "customer"`) };
+
+    if (id_user == "") { return res.status(400).send(`Please input id_user`) };
     const checkId_user = await model.findId_user(id_user);
-    if (checkId_user == 0) {
-      return res.status(400).send(`Id_user not found`);
-    }
+    if (checkId_user.rowCount == 0) { return res.status(400).send(`Id_user not found`) };
 
-    const { id_ticket, total_passenger, total_payment } = req.body;
-
-    if (id_ticket == "") {
-      return res.status(400).send(`Please input id_ticket`);
-    }
-    if (isNaN(id_ticket)) {
-      return res.status(400).send(`id_ticket must be a Number`);
-    }
+    if (id_ticket == "") { return res.status(400).send(`Please input id_ticket`) };
+    if (isNaN(id_ticket)) { return res.status(400).send(`id_ticket must be a Number`) };
     const checkId_ticket = await model.findId_ticket(id_ticket);
-    if (checkId_ticket == 0) {
-      return res.status(400).send(`Id_ticket not found`);
-    }
+    if (checkId_ticket.rowCount == 0) { return res.status(400).send(`id_ticket not found`) };
 
-    if (total_passenger == "") {
-      return res.status(400).send(`Please input total_passenger`);
-    }
-    if (isNaN(total_passenger)) {
-      return res.status(400).send(`total_passenger must be a Number`);
-    }
+    if (total_passenger == "") { return res.status(400).send(`Please input total_passenger`) };
+    if (isNaN(total_passenger)) { return res.status(400).send(`total_passenger must be a Number`) };
 
-    if (isNaN(total_payment)) {
-      return res
-        .status(400)
-        .send(`total_payment must be Input and data-type Number`);
-    }
+    if (isNaN(total_payment)) { return res.status(400).send(`Please input total_payment with data-type Number`) };
 
     const status_payment = "waiting";
 
-    // total payment dari pronent, ubah controller dan model
-
-    await model.post(
-      id_user,
-      id_ticket,
-      total_payment,
-      total_passenger,
-      status_payment
-    );
+    await model.post( id_user, id_ticket, total_passenger, total_payment, status_payment, );
 
     res.status(200).json({
       id_user: id_user,
       id_ticket: id_ticket,
-      total_payment: total_payment,
       total_passenger: total_passenger,
+      total_payment: total_payment,
       status_payment: status_payment,
     });
   } catch (error) {
@@ -149,18 +118,12 @@ const post = async (req, res) => {
 // DELETE BOOKING BY ID_BOOKING
 const deletebyid = async (req, res) => {
   try {
-    const { id_user, id_booking } = req.body;
+    const { id_booking } = req.body;
+    if (isNaN(id_booking)) { return res.status(400).send(`id_booking must be a Number`) };
     const idforconsole = id_booking;
 
-    if (id_user == "") { return res.status(400).send(`Please input id_user`) };
-    const checkId_user = await model.findId_user(id_user);
-    if (checkId_user == 0) { return res.status(400).send(`Id_user not found`) };
-    
     const getIdBooking = await model.findbyId(id_booking);
-    if (getIdBooking?.rowCount == 0) { return res.status(400).send("Id_booking tidak ditemukan") };
-
-    const userAllBooking = await model.showAllByIdBookingIdUser(id_booking, id_user);
-    if (userAllBooking?.rowCount == 0) { return res.status(400).send("This is not your id_booking") };
+    if (getIdBooking?.rowCount == 0) { return res.status(400).send("id_booking tidak ditemukan") };
 
     await model.deletebyid(id_booking);
     res.send(`data id_booking ke-${idforconsole} berhasil dihapus`);
@@ -181,8 +144,8 @@ const statuspaymentV2 = async (req, res) => {
     if (isNaN(id_booking)) {return res.status(400).send(`id_booking must be a Number`) };
     const getData = await model.findbyId(id_booking);
     if (getData?.rowCount == 0) {return res.status(400).send("id_booking tidak ditemukan") };
-    if ( getData?.rows[0].status_payment == "canceled" ) { 
-      return res.status(400).send(`Can't change if status_payment: "canceled"`) 
+    if (getData?.rows[0].status_payment == "canceled" ) { 
+      return res.status(400).send(`Can't change status_payment from "canceled" to another status`) 
     };
 
     if (
@@ -195,16 +158,16 @@ const statuspaymentV2 = async (req, res) => {
 
     if (getData?.rows[0].status_payment === "waiting") {
       if (status_payment != "issue") {
-        return res.status(400).send(`Admin can upgrade this status_payment to "issue"`);
+        return res.status(400).send(`Admin can upgrade status_payment from "waiting" to "issue"`);
       }
     }
     if (getData?.rows[0].status_payment === "issue") {
       if (status_payment != "boarding") {
-        return res.status(400).send(`Admin can upgrade this status_payment to "boarding"`);
+        return res.status(400).send(`Admin can upgrade status_payment from "issue" to "boarding"`);
       }
     }
     if (getData?.rows[0].status_payment === "boarding") {
-      return res.status(400).send(`Status_payment is "boarding", can't upgrade status_payment more than "boarding"`);
+      return res.status(400).send(`now status_payment is "boarding", can't upgrade status_payment more than "boarding"`);
     }
 
     const patchData = await model.statuspayment(status_payment, id_booking);
@@ -242,11 +205,14 @@ const statuspaymentV2 = async (req, res) => {
 // PATCH DATA TO EDIT STATUS PAYMENT FOR CANCELED BOOKING TICKET, all role can Access.
 const statuspaymentV3 = async (req, res) => {
   try {
-    const { id_booking } = req.body;
+    const { id_booking, id_user } = req.body;
 
     if (isNaN(id_booking)) {return res.status(400).send(`id_booking must be a Number`) };
     const getData = await model.findbyId(id_booking);
     if (getData?.rowCount == 0) {return res.status(400).send("id_booking tidak ditemukan") };
+    if (isNaN(id_user)) { return res.status(400).send(`id_user must be a Number`) };
+    const userAllBooking = await model.showAllByIdBookingIdUser(id_booking, id_user);
+    if (userAllBooking?.rowCount == 0) { return res.status(400).send(`This id_booking not created by customer id_user: ${id_user}`) };
 
     if(getData?.rows[0].status_payment !== "waiting") {
       return res.status(400).send(`This route can just "canceled" status_payment from "waiting`);
