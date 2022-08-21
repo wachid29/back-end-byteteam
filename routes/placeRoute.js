9f8e3b07d101dc1b7df0f20a295bb2f437bbe6a9
wachid <wachidnurafif29@gmail.com>
2022-08-21 21:03:52 +0700

    ok

--- routes/placeRoute.js
+++ routes/placeRoute.js
@@ -5,5 +5,6 @@ Router.get("/place", controller.getPlaces);
 Router.post("/place/add", controller.addPlace);
 Router.get("/place/find-city", controller.findByCity);
 Router.delete("/place/delete", controller.deletePlace);
+Router.get("/place/findbyID", controller.findByID);
 
 module.exports = Router;
