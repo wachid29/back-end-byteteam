2d71e93f2b1d089518de9cc32ee50ef722299409
wachid <wachidnurafif29@gmail.com>
2022-08-22 16:05:05 +0700

    ok

--- routes/placeRoute.js
+++ routes/placeRoute.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const controller = require("../controllers/placeController");
 
 Router.get("/place", controller.getPlaces);
-Router.get("/place-fix", controller.getPlaces);
+Router.get("/place-fix", controller.getPlacesfix);
 Router.post("/place/add", controller.addPlace);
 Router.get("/place/find-city", controller.findByCity);
 Router.delete("/place/delete", controller.deletePlace);
